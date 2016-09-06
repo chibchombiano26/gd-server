dd044f7371c44b9fb313bfbe04ced8500a508134
Jose douglas ramirez e <jose.douglas.ramirez@outllok.com>
2016-09-06 03:00:04 -0500

    server

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,10 @@ WORKDIR /usr/src/app
 COPY package.json /usr/src/app/package.json
 
 RUN npm install
+RUN npm install typings --global
+RUN typings install
+RUN npm install typescript --global
+RUN tsc
 COPY . /usr/src/app
 
 EXPOSE 8081
--- express/server.ts
+++ express/server.ts
@@ -18,8 +18,10 @@ export class expressServer{
         
         app.use(express.static('./'));
         
-        app.use(function (req, res, next) {            
+        app.use(function (req, res, next) {
+            res.setHeader('Access-Control-Allow-Origin', '*');            
             res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+            res.setHeader('Access-Control-Allow-Origin', 'http://gd-client.65b7caeb.svc.dockerapp.io');            
             res.setHeader('Access-Control-Allow-Methods', 'POST');
             res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
             res.setHeader('Access-Control-Allow-Credentials', 'true');
